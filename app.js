520943134f4d33de9380ff310557f1e64fbbd75b
minjeong-dev <vitaminjeong1026@gmail.com>
2022-04-28 01:38:28 +0900

    전체 체크 및 삭제 기능 추가, 추가한 리스트로 스크롤링, 체크한것 맨 밑으로 보내기

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 /**
- * 전체 삭제 기능
- * 추가 아이템 스크롤링
+ * 전체 체크, 삭제 기능 ⭕
+ * 추가 아이템 스크롤링 ⭕
  * 스크롤바 디자인
  * 가격 합산 기능
  * 저장 기능(localstorage)
- * 체크한 것 아래로 보내기
+ * 체크한 것 아래로 보내기 ⭕
  * 우선순위 필터링 및 위로 보내고, 구분된 디자인
  * 코드 효율적으로 개선 ⭕
  */
 
 const lists = document.querySelector('.lists');
 const form = document.querySelector('form');
+const controlBtn = document.querySelector('.control-box');
 const inputText = document.querySelector('.input-text');
 const submitBtn = document.querySelector('.submit-btn');
 
@@ -23,6 +24,7 @@ function onSubmit(event) {
   }
   const list = addList(newList);
   lists.appendChild(list);
+  list.scrollIntoView({ block: 'center' });
   inputText.value = '';
   inputText.focus();
 }
@@ -33,7 +35,7 @@ function addList(newList) {
   list.setAttribute('class', 'list-row');
   list.setAttribute('data-id', id);
   list.innerHTML = `
-    <div class="list">
+    <div id="list">
       <button class="check-btn">✔</button>
       <span class="span-text">${newList}</span>
       <button class="remove-btn">
@@ -44,20 +46,38 @@ function addList(newList) {
   id++;
   return list;
 }
-function onCheck(event) {
-  const checkList = event.target.parentElement;
-  event.target.checked
-    ? (checkList.style.color = 'rgb(209, 209, 209)')
-    : (checkList.style.color = 'black');
-}
+
+controlBtn.addEventListener('click', (event) => {
+  const targetClass = event.target.parentElement.className;
+  const listAll = lists.childNodes;
+  if (targetClass === 'ckeck-all') {
+    for (let i = 0; i < listAll.length; i++) {
+      listAll[i].childNodes[1].classList.toggle('checked');
+    }
+  } else if (targetClass === 'remove-all') {
+    lists.innerHTML = '';
+  } else {
+    return;
+  }
+});
 form.addEventListener('submit', onSubmit);
 lists.addEventListener('click', (event) => {
   const dataId = event.target.dataset.id;
+  const targetClass = event.target.className;
   if (dataId) {
+    // remove 버튼 클릭
     const removeList = document.querySelector(`.list-row[data-id="${dataId}"]`);
     removeList.remove();
-  } else {
+  } else if (event.target.id === 'list') {
+    // 나머지 부분 클릭
+    const checkList = event.target;
+    checkList.classList.toggle('checked');
+    checkList.parentElement.remove();
+    lists.appendChild(checkList.parentElement);
+  } else if (targetClass === 'check-btn' || targetClass === 'span-text') {
     const checkList = event.target.parentElement;
     checkList.classList.toggle('checked');
+    checkList.parentElement.remove();
+    lists.appendChild(checkList.parentElement);
   }
 });
